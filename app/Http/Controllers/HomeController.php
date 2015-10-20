3d676ea4ca53d69ba4af4c13ebe022be5f08e13a
Mary Gale <mary.jabagat@corp.mobile.co>
2015-10-20 08:40:53 +0800

    add app

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+
+class HomeController extends Controller{
+
+    public function index(){
+        return view('home');
+    }
+}
+
