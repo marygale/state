5ec2d4f8178a0225aee8dbe2b863896c7b56c6c0
Mary Gale <mary.jabagat@corp.mobile.co>
2015-11-13 23:58:06 +0800

    - fixed errors on homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,7 @@ use Illuminate\Routing\Controller;
 class HomeController extends Controller{
 
     public function index(){
-        return view('home');
+        return view('welcome');
     }
 }
 
--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'ukvRzvoJPTquzYPL4KRLXJt4ePPvJYKD'),
 
     'cipher' => 'AES-256-CBC',
 
